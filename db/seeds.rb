6ee988e4d7b1a185e5af801ceaa597d364b92cfa
josediaz16 <jldiaz1612@gmail.com>
2018-01-27 18:32:56 -0500

    fix server launch

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+quiz = Quiz.create(name: "Friends trivia", category: "TV show")
+Question.create(quiz: quiz, description: "Name of Monica's brother", options: ['Chandler', 'Ross', 'Joey', 'Mike'], answer: "Ross")
+Question.create(quiz: quiz, description: "Who says vafanapoli?", options: ['Rachel', 'Monica', 'Joey', 'Mike'], answer: "Joey")
+Question.create(quiz: quiz, description: "How many sisters does Joey have?", options: ['7', '3', '4', '0'], answer: "7")
+Question.create(quiz: quiz, description: "Almost beat pacman's record", options: ['Jack', 'Ben', 'Gunther', 'Phoebe'], answer: "Phoebe")
+Question.create(quiz: quiz, description: "Who says 'I'm not great at the advice. Can I interest you in a sarcastic comment?'", options: ['Chandler', 'Rachel', 'Joey', 'Frankie Jr'], answer: "Chandler")
