df890d8af731e1c345ccfc01888806b361f5055b
josediaz16 <jldiaz1612@gmail.com>
2018-01-27 19:52:19 -0500

    add basic error cases on create quiz

--- spec/acceptance/quiz/create_spec.rb
+++ spec/acceptance/quiz/create_spec.rb
@@ -53,51 +53,114 @@ RSpec.describe QuizzesController, type: :request do
     end
 
     post "/quizzes" do
-      let(:raw_post) { params.to_json }
+      context "A successful request" do
+        let(:raw_post) { params.to_json }
 
-      example "Creating a quiz" do
-        expect(params).to eq({
-          "quiz"=> {
-            "name"=>"Friends Trivia",
-            "category"=>"TV show",
-            "questions_attributes"=> [
-              {
-                "description"=>"Name of Monica's brother",
-                "options"=>["Chandler", "Ross", "Joey", "Mike"],
-                "answer"=>"Ross"
-              },
-              {
-                "description"=>"Who says vafanapoli?",
-                "options"=>["Rachel", "Monica", "Joey", "Mike"],
-                "answer"=>"Joey"
-              },
-              {
-                "description"=>"How many sisters does Joey have?",
-                "options"=>["7", "3", "4", "0"],
-                "answer"=>"7"
-              },
-              {
-                "description"=>"Almost beat pacman's record",
-                "options"=>["Jack", "Ben", "Gunther", "Phoebe"],
-                "answer"=>"Phoebe"
-              },
-              {
-                "description"=>
-                "Who says 'I'm not great at the advice. Can I interest you in a sarcastic comment?'",
-                "options"=>["Chandler", "Rachel", "Joey", "Frankie Jr"],
-                "answer"=>"Chandler"
-              }
-            ]
-          }
-        })
+        example "Creating a quiz" do
+          expect(params).to eq({
+            "quiz"=> {
+              "name"=>"Friends Trivia",
+              "category"=>"TV show",
+              "questions_attributes"=> [
+                {
+                  "description"=>"Name of Monica's brother",
+                  "options"=>["Chandler", "Ross", "Joey", "Mike"],
+                  "answer"=>"Ross"
+                },
+                {
+                  "description"=>"Who says vafanapoli?",
+                  "options"=>["Rachel", "Monica", "Joey", "Mike"],
+                  "answer"=>"Joey"
+                },
+                {
+                  "description"=>"How many sisters does Joey have?",
+                  "options"=>["7", "3", "4", "0"],
+                  "answer"=>"7"
+                },
+                {
+                  "description"=>"Almost beat pacman's record",
+                  "options"=>["Jack", "Ben", "Gunther", "Phoebe"],
+                  "answer"=>"Phoebe"
+                },
+                {
+                  "description"=>
+                  "Who says 'I'm not great at the advice. Can I interest you in a sarcastic comment?'",
+                  "options"=>["Chandler", "Rachel", "Joey", "Frankie Jr"],
+                  "answer"=>"Chandler"
+                }
+              ]
+            }
+          })
 
-        do_request
+          do_request
 
-        quiz = Quiz.last
+          quiz = Quiz.last
 
-        expect(status).to eq(200)
-        response = JSON.parse(response_body)
-        expect(response). to eq({"id" => quiz.id})
+          expect(status).to eq(200)
+          response = JSON.parse(response_body)
+          expect(response). to eq({"id" => quiz.id})
+        end
+      end
+
+      context "Sending a blank name" do
+        let!(:name) { "" }
+        let(:raw_post) { params.to_json }
+
+        example "Should respond 400", document: false do
+          do_request
+
+          quiz = Quiz.last
+
+          expect(status).to eq(400)
+          expect(quiz).to be_nil
+
+          response = JSON.parse(response_body)
+          expect(response).to eq({
+            "error" => {
+              "name" => ["can't be blank"]
+            }
+          })
+        end
+      end
+      context "Sending an empty array of questions" do
+        let!(:questions_attributes) { [] }
+        let(:raw_post) { params.to_json }
+
+        example "Should respond 400", document: false do
+          do_request
+
+          quiz = Quiz.last
+
+          expect(status).to eq(400)
+          expect(quiz).to be_nil
+
+          response = JSON.parse(response_body)
+          expect(response).to eq({
+            "error" => {
+              "questions" => ["can't be blank"]
+            }
+          })
+        end
+      end
+      context "Sending a question without answer" do
+        before { questions_attributes[0].delete("answer") }
+        let(:raw_post) { params.to_json }
+
+        example "Should respond 400", document: false do
+          do_request
+
+          quiz = Quiz.last
+
+          expect(status).to eq(400)
+          expect(quiz).to be_nil
+
+          response = JSON.parse(response_body)
+          expect(response).to eq({
+            "error" => {
+              "questions.answer" => ["can't be blank"]
+            }
+          })
+        end
       end
     end
   end
