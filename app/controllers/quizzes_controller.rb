81588063650f373630e77dd57c39f523ac3c1990
josediaz16 <jldiaz1612@gmail.com>
2018-01-27 21:31:02 -0500

    adds implementation to update a quiz

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -11,6 +11,15 @@ class QuizzesController < ApplicationController
   def create
     quiz = Quiz.new(quiz_params)
     if quiz.save
+      render json: {id: quiz.id}, status: 201
+    else
+      render json: {error: quiz.errors.messages}, status: 400
+    end
+  end
+
+  def update
+    quiz = Quiz.find(params[:id])
+    if quiz.update(quiz_params)
       render json: {id: quiz.id}
     else
       render json: {error: quiz.errors.messages}, status: 400
@@ -20,6 +29,6 @@ class QuizzesController < ApplicationController
   private
 
   def quiz_params
-    params.require(:quiz).permit(:name, :category, questions_attributes: [:description, :answer, options: []])
+    params.require(:quiz).permit(:name, :category, questions_attributes: [:id, :description, :answer, options: []])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :quizzes, only: [:index, :show, :create] do
+  resources :quizzes, only: [:index, :show, :create, :update] do
     resources :questions, only: :show
   end
 end
